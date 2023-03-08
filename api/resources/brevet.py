c1a4fda624aa3fb93d2d85aa323111b375571cb8
vboxuser <simonz@uoregon.edu>
2023-03-08 10:30:45 -0800

    Added RESTful API

--- api/resources/brevet.py
+++ api/resources/brevet.py
@@ -7,6 +7,23 @@ from flask_restful import Resource
 # You need to implement this in database/models.py
 from database.models import Brevet
 
+class Brevet(Resource):
+
+    def get(self, id):
+        brevet = Brevet.objects.get(id = id).to_json()
+        return Response(brevet, mimetype = "application/json", status = 200)
+    
+    def put(self, id):
+        input_json = request.json
+        Brevet.objects.get(id = id).update(**input_json)
+        return "", 200
+    
+    def delete(self, id):
+        Brevet.objects.get(id = id).delete()
+        return "", 200
+
+
+
 # MongoEngine queries:
 # Brevet.objects() : similar to find_all. Returns a MongoEngine query
 # Brevet(...).save() : creates new brevet
